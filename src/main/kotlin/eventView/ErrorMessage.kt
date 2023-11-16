73752bf684a4cc45b0d6ea1310eec4bae459ff42
park-yina <111878820+park-yina@users.noreply.github.com>
2023-11-16 17:14:08 +0900

    Revert "pull_request를 위한 브런치 생성입니다."

--- src/main/kotlin/eventView/ErrorMessage.kt
+++
@@ -1,8 +0,0 @@
-package eventView
-
-enum class ErrorMessage(val message: String) {
-   INVALID_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-   DUPLICATE_MENU("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
-   INVALID_MENU_FORM("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
-   OVER_MENU("[ERROR]메뉴는 한번에 20개까지만 주문 가능합니다.")
-}
