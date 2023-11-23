32ad84e7695122edd5466b2ec5518a014756c88a
park-yina <111878820+park-yina@users.noreply.github.com>
2023-11-23 15:35:32 +0900

    리팩토링: ValidInput.kt의 15줄 넘는 메서드 수정

--- src/main/kotlin/eventViewModel/ValidInput.kt
+++ src/main/kotlin/eventViewModel/ValidInput.kt
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console
 import eventView.ErrorMessage
 import eventView.Input
 import eventView.ValidInputDatas
+import eventViewModel.ValidInput.Parsing.BAR
+import eventViewModel.ValidInput.Parsing.COMMA
 
 
 class ValidInput {
@@ -20,20 +22,20 @@ class ValidInput {
             "디저트" to listOf(Pair("초코케이크", 15000), Pair("아이스크림", 5000)),
             "음료" to listOf(Pair("제로콜라", 3000), Pair("레드와인", 60000), Pair("샴페인", 25000))
         )
-        val menuMap= mutableMapOf<String,Int>()
-        var totalPrice=0
-        var benefitPrice=0
-        var giveway=false
-        var inputDay=0
-        var dayType=false
-        var weekEndDiscount=0
-        var workdayDiscount=0
-        var benefitList= mutableListOf<String>()
-        var d_DayDiscount=0
-        var result=0
+        val menuMap = mutableMapOf<String, Int>()
+        var totalPrice = 0
+        var benefitPrice = 0
+        var giveway = false
+        var inputDay = 0
+        var dayType = false
+        var weekEndDiscount = 0
+        var workdayDiscount = 0
+        var benefitList = mutableListOf<String>()
+        var d_DayDiscount = 0
+        var result = 0
     }
 
-    fun date_Verification():Int{
+    fun date_Verification(): Int {
         val date = Console.readLine()
         when {
             date.toIntOrNull() == null -> {
@@ -45,32 +47,27 @@ class ValidInput {
             }
         }
         Input().printlnOrderMenu()
-         inputDay=date.toInt()
+        inputDay = date.toInt()
         return inputDay
     }
+
     fun pharsing_InputData(): List<String> {
         val inputMenu = Console.readLine()
-        return inputMenu.split(",")
+        return inputMenu.split(COMMA)
     }
 
-    fun checking_InputData(){
+    fun checking_InputData() {
         val input = pharsing_InputData()
         input.forEach {
-            if (!it.contains("-")) {
-                throw IllegalArgumentException(ErrorMessage.INVALID_MENU_FORM.message)
-            }
-            val (key, value) = it.split("-")
-
-            if (input.count { item -> item.split("-")[0].equals(key) } > NumberLimit.DUPLICATE.value) {
+            findBar(it)
+            val (key, value) = it.split(BAR)
+            if (input.count { item -> item.split(BAR)[0] == key } > NumberLimit.DUPLICATE.value) {
                 throw IllegalArgumentException(ErrorMessage.DUPLICATE_MENU.message)
             }
             if (menu.none { (_, items) -> items.any { it.first == key } }) {
                 throw IllegalArgumentException(ErrorMessage.INVALID_MENU_FORM.message)
             }
-
-            if (value.toIntOrNull() == null) {
-                throw IllegalArgumentException(ErrorMessage.INVALID_MENU_FORM.message)
-            }
+            convertInt(value)
             if (input.all { it.split("-")[0] in menu[SerachingKey.DRINKING.key]!!.map { it.first } }) {
                 throw IllegalArgumentException(ErrorMessage.INVALID_MENU_FORM.message)
             }
@@ -79,5 +76,23 @@ class ValidInput {
             }
             menuMap[key] = value.toInt()
         }
+    }
+
+    private fun convertInt(value: String) {
+        if (value.toIntOrNull() == null) {
+            throw IllegalArgumentException(ErrorMessage.INVALID_MENU_FORM.message)
+        }
+    }
+
+    private fun findBar(it: String) {
+        if (!it.contains(BAR)) {
+            throw IllegalArgumentException(ErrorMessage.INVALID_MENU_FORM.message)
+        }
+    }
+
+    private object Parsing {
+        const val BAR = "-"
+        const val COMMA = ","
+
     }
 }
