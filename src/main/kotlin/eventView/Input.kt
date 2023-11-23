8dbdad353a035d623d3305d28565a63efb039598
park-yina <111878820+park-yina@users.noreply.github.com>
2023-11-23 15:44:09 +0900

    리팩토링: 함수명 변경 및 Input.kt에 return try구문 사용

--- src/main/kotlin/eventView/Input.kt
+++ src/main/kotlin/eventView/Input.kt
@@ -3,34 +3,34 @@ package eventView
 import eventViewModel.ValidInput
 
 class Input {
-    private val validInput=ValidInput()
-    private fun helloMent(){
+    private val validInput = ValidInput()
+    private fun helloMent() {
         println(InputMent.HELLO_USER.message)
     }
-    private fun printlnVisitDate(){
+
+    private fun printlnVisitDate() {
         println(InputMent.READ_EXPECTED_VISIT_DATE.message)
     }
-    fun dateErrorPrintln():Int{
+
+    fun readInputDate(): Int {
         helloMent()
         printlnVisitDate()
-        val date:Int
-        try{
-            date=validInput.date_Verification()
-        }
-        catch (e:IllegalArgumentException){
+        return try {
+            validInput.date_Verification()
+        } catch (e: IllegalArgumentException) {
             println(e.message)
-            return validInput.date_Verification()
+            validInput.date_Verification()
         }
-        return date
     }
-    fun printlnOrderMenu(){
+
+    fun printlnOrderMenu() {
         println(InputMent.ORDER_MENU_MENT.message)
     }
-    fun menuErrorPrintln(){
-        try{
+
+    fun menuErrorPrintln() {
+        try {
             validInput.checking_InputData()
-        }
-        catch (e:IllegalArgumentException){
+        } catch (e: IllegalArgumentException) {
             println(e.message)
             return validInput.checking_InputData()
         }
